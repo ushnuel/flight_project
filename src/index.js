c9471446c8640d833de39181089787634071dcff
Emmanuel <ezeemmanuel2010@gmail.com>
2019-12-04 10:16:09 +0100

    chore(cors): add cors to server header

--- src/index.js
+++ src/index.js
@@ -10,14 +10,13 @@ const app = express();
 app.use(cors());
 app.use(json());
 app.use(urlencoded({ extended: false }));
-
-app.use('/v1/flight', Route);
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Authorization, Content-Type');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE');
   next();
 });
+app.use('/v1/flight', Route);
 app.get('/', (req, res, next) => {
   res.status(200).json({
     message: 'Welcome to our flight search api server',
